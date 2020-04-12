2c1a45e68da418de1f708328c3aaf4bee64168b8
Noldor5150 <ruikis.paulius@gmail.com>
2020-04-12 20:44:27 +0300

    test commit

--- HomeWork03/Program.cs
+++ HomeWork03/Program.cs
@@ -6,7 +6,7 @@ namespace HomeWork03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World TEst One!");
         }
     }
 }
